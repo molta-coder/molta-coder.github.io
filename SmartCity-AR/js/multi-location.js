da386d1aea261ffede04ca724fe725cc48df9dee
molta-coder <63711730+molta-coder@users.noreply.github.com>
2021-03-01 00:24:49 +0100

    Update multi-location.js

--- SmartCity-AR/js/multi-location.js
+++ SmartCity-AR/js/multi-location.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 47.4729928,
+                lng: 19.0517917,
             },
         },
     ];
